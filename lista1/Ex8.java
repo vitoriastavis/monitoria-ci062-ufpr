52a0adedd6eda9ef646206c78d83ffc6d6b97e7e
vitoriastavis <vitoriastavisa@gmail.com>
2022-10-24 23:07:50 -0300

    fazendo lista 2

---
+++ lista1/Ex8.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Ex8
+{
+    // le peso e altura de pessoas ate digitar -1 e retorna quantas estao acima do peso
+    public static void main(String args[])
+    {
+        // declara variaveis
+        int count;              // contador de pessoas acida do peso
+        double peso, altura, imc;
+        Scanner input = new Scanner(System.in);
+
+        count = 0;
+        imc = 0;
+
+        // le peso e altura
+        System.out.println("digite o peso em kg");   
+        peso = input.nextDouble(); 
+
+        while (peso != -1)
+        {
+            System.out.println("digite a altura em m"); 
+            altura = input.nextDouble();
+
+            if (altura == -1)
+                break;
+
+            imc = peso/(altura*altura);
+
+            if (imc > 25)
+            {
+                count += 1;                
+            }
+
+            System.out.println("digite o peso");   
+            peso = input.nextDouble();            
+        }
+
+        System.out.printf("existem %d pessoas acima do peso \n", count);
+      
+        input.close();
+    }       
+}
+
---
+++ lista2.1-arrays/Ex1.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Ex1
+{
+    // le 10 numeros e retorna:
+    // numeros pares
+    // soma dos numeros pares
+    // numeros numeros
+    // quantidade de numeros numeros
+    public static void main(String args[])
+    {
+        // declara variaveis
+        int numeros[], qtdImpares, somaPares;
+        Scanner input = new Scanner(System.in);
+
+        numeros = new int[10];
+        qtdImpares = somaPares = 0;
+
+        // le numeros e salva no array
+        System.out.println("digite 10 numeros");       
+
+        for (int i = 0; i < 10; i++)      
+        {  
+            numeros[i] = input.nextInt();      
+                               
+        }
+
+        for (int i = 0; i < 10; i++)      
+        {  
+            if (numeros[i] % 2 == 0)
+            {
+                System.out.printf("%d ", numeros[i]);
+                somaPares += numeros[i];
+            }  
+            
+        }
+
+        System.out.printf("\na soma dos numeros pares eh %d \n", somaPares);
+
+        for (int i = 0; i < 10; i++)      
+        {  
+            if (numeros[i] % 2 != 0)
+            {
+                System.out.printf("%d ", numeros[i]);
+                qtdImpares++;               
+            }              
+        }
+        
+        System.out.printf("\nforam escritos %d numeros impares \n", qtdImpares);
+                      
+        input.close();
+    }    
+    
+
+}
+
