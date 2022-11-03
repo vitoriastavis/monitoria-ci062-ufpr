fe04edbac8b3b547111cae0ad76823389eec7ddc
vitoriastavis <vitoriastavisa@gmail.com>
2022-11-03 20:43:05 -0300

    terceira

---
+++ lista3/Data.java
@@ -0,0 +1,6 @@
+public class Data
+{
+    int dia;
+    int mes;
+    int ano;
+}
---
+++ lista3/FuncionarioUFPR.java
@@ -0,0 +1,9 @@
+package Data;
+
+public class FuncionarioUFPR
+{
+    String nome;
+    String cargo;
+    int matricula;
+    Data dataNascimento;
+}
